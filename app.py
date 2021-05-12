e2266bf6ac20f9d8327f28eacb842a46f356a1a8
evileroy <71393886+evileroy@users.noreply.github.com>
2021-05-12 22:51:23 +0200

    upload file

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from azure.storage.blob import BlobClient
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+
+account_name = "qwirklefunction"
+account_key = "KaHc+hHr/rVqLCoMu74h8ahUJtfQysPqDNpyeazVfSGWl04HhkrGBKnYmIw+/LwqjIXOMm/1krKQnFdyKNOkjQ=="
+container_name = "spelbord"
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,13 +14,17 @@ def hello():
 @app.route('/upload')
 def upload_file():
    return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
+
+@app.route("/uploader", methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save("./blob.jpg")
+        blob = BlobClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=qwirklefunction;AccountKey=KaHc+hHr/rVqLCoMu74h8ahUJtfQysPqDNpyeazVfSGWl04HhkrGBKnYmIw+/LwqjIXOMm/1krKQnFdyKNOkjQ==;EndpointSuffix=core.windows.net", "spelbord", "my_blob")
+        with open("./blob.jpg", "rb") as data:
+            blob.upload_blob(data, overwrite=True)
+        return "Afbeelding werd geüpload."
+
+
 if __name__ == '__main__':
    app.run(debug = True)
